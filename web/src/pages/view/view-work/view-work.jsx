132417c507090cbf7126ac56906463aa934660ae
AndreiZg <zgherea13@gmail.com>
2024-05-15 17:03:22 +0200

    feat/view work page added

--- web/src/pages/view/view-work/view-work.jsx
+++ web/src/pages/view/view-work/view-work.jsx
@@ -1,11 +1,15 @@
 import NavBar from "../../../components/navbar/navbar";
 import WorkDetails from "../work-details/work-details";
+import WorkImages from "../work-images/work-images";
+import Footer from "../../../components/footer/footer";
 
 const ViewWork = () => {
   return(
       <>
           <NavBar/>
           <WorkDetails/>
+          <WorkImages/>
+          <Footer/>
       </>
   )
 }
---
+++ web/src/pages/view/work-images/work-images.jsx
@@ -0,0 +1,18 @@
+import './work-images.css';
+
+const WorkImages = () =>{
+    return(
+        <>
+        <div className="work-images flex">
+            <div className="images-container">
+                <div className="work-image"></div>
+                <div className="work-image"></div>
+                <div className="work-image"></div>
+                <div className="work-image"></div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default WorkImages;
